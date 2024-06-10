aea0560db808f76bd9beca957aa28c46a30fb715
edsoncuno4@gmail.com <edsoncuno4@gmail.com>
2024-06-10 17:03:56 +0000

    descargado los cambios de action y mi cambio en la configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://<USERNAME>.github.io/<REPO>/',
+  base: '/<REPO>/',
   plugins: [vue()],
 })
